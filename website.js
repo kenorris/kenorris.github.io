08297462e798ee83df8ad2eaf9c956575b163621
kenorris <kellynnorris2018@u.northwestern.edu>
2017-09-22 12:16:52 -0500

    ok

--- website.js
+++ website.js
@@ -1,102 +1,32 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-	var slideIndex = 0;
-showSlides();
 
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; 
+  var text = ["journalist", "storyteller", "producer", "leader"];
+  var counter = 0;
+  var elem = document.getElementById("changeText");
+  setInterval(change, 1500);
+
+  function change() {
+  elem.innerHTML = text[counter];
+  counter++;
+  if (counter >= text.length) {
+    counter = 0;
+  }
+}
+
+  $(window).scroll(function () {
+      // Get the height of the banner,
+      // and then set your menu.
+      var bannerHeight = $('.banner').height();
+      console.log(bannerHeight);
+    if ($(window).scrollTop() > bannerHeight) {
+      $('#nav_bar').addClass('navbar-fixed');
     }
-    slideIndex++;
-    if (slideIndex> slides.length) {slideIndex = 1} 
-    slides[slideIndex-1].style.display = "block"; 
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
-
-function preloadImage(src="HeadPhoto1.png")
-{
-    var img=new Image();
-    img.src=url;
-}
-
-	//main layout
-
-	$('#clips').click(function () {
-        $('.clips-info').slideToggle(300);
-        $('#production').hide ();
-		$('#videoreporting').hide ();
-		$('#writing').hide ();
-
-        $(".contact-info").hide();
-        $(".resume-info").hide();
-     });
-
-	$('#resume').click(function () {
-        $('.resume-info').slideToggle(300);
-        $(".contact-info").hide();
-        $(".clips-info").hide();
-     });
-
-	$('#contact').click(function () {
-        $('.contact-info').slideToggle(300);
-        $(".resume-info").hide();
-        $(".clips-info").hide();
-
-     });
-
-    $(".scroll").click(function(event){   
-      event.preventDefault();
-      $('html,body').animate({scrollTop:$(this.hash).offset().top}, 700);
-    });
+    if ($(window).scrollTop() < bannerHeight) {
+      $('#nav_bar').addClass('navbar-fixed');
+    }
 
 
-//clips tabs
-	$('#box1').click(function () {
-        $('#production').slideToggle(300);
-		$('#videoreporting').hide ();
-		$('#writing').hide ();
-     });
-
-	$('#box2').click(function () {
-        $('#videoreporting').slideToggle(300);
-        $('#production').hide ();
-        $('#writing').hide ();
-     });
-
-	$('#box3').click(function () {
-        $('#writing').slideToggle(300);
-        $('#production').hide ();
-        $('#videoreporting').hide ();
-     });
-
-    $(".scroll").click(function(event){   
-      event.preventDefault();
-      $('html,body').animate({scrollTop:$(this.hash).offset().top}, 700);
-    });
-	//portfolio
-
-	$("#production").hide();
-	$("#videoreporting").hide();
-	$("#writing").hide();
-		//fades
-
-	$("#journolink").on("click",function(){
-		$("#journo").fadeIn("slow");
-		$("#digital").hide();
-	});
-
-	$("#digitallink").on("click",function(){
-		$("#journo").hide();
-		$("#digital").fadeIn("slow");
-	});
-
-		//click
-
-	$(".boxy").click(function() {
-	   $(".boxy").removeClass("active");  // remove active class from all
-	   $(this).addClass("active");         // add active class to clicked
-	});
-
+  });
 });
+
